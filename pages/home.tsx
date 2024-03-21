450e42fed6a1dcdf59906592e8a541ebdaf350da
Gino Leone <163489658+cockymousprime@users.noreply.github.com>
2024-03-20 19:52:36 -0600

    Working - formatted array and added new contract

---
+++ pages/home.tsx
@@ -0,0 +1,5 @@
+function MyApp() {
+  return <h1>hello world</h1>;
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,59 @@
-import { ConnectWallet, Web3Button, useAddress, useContract, useTokenBalance } from "@thirdweb-dev/react";
+import {
+  ConnectWallet,
+  Web3Button,
+  useAddress,
+  useContract,
+  useTokenBalance,
+} from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
 import { NextPage } from "next";
 import { useState, useEffect } from "react";
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+import { BigNumber, utils } from "ethers";
 
 const Home: NextPage = () => {
   const [merkleRoot, setMerkleRoot] = useState<string | null>(null);
-  const [proofsData, setProofsData] = useState<string[]>([]);
+  const [proofsData, setProofsData] = useState<string[]>();
   const address = useAddress();
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        console.log("fetching proofs for address:" + address);
-        const response = await fetch('/tree.json');
-        const treeData = await response.json();
+  async function fetchData() {
+    try {
+      console.log("fetching proofs for address:" + address);
+      const response = await fetch("/tree.json");
+      const treeData = await response.json();
 
-        const tree = StandardMerkleTree.load(treeData);
-        console.log("JSON loaded.");
+      const tree = StandardMerkleTree.load(treeData);
+      console.log("JSON loaded.");
 
-        const proofs = [];
-        console.log("loading proofs for:" + address);
-        for (const [i, v] of tree.entries()) {
-          if (v[0] === address) {
-            const proof = tree.getProof(i);
-            proofs.push(proof);
-          }
+      setMerkleRoot(tree.root);
+
+      const proofs = [];
+      console.log("loading proofs for:" + address);
+
+      for (const [i, v] of tree.entries()) {
+        if (v[0] === address) {
+          const proof = tree.getProof(i);
+          proofs.push(proof);
         }
-        setProofsData(proofs); // Update proofsData state
-      } catch (error) {
-        console.log("we had an error, panic" + error);
       }
+      setProofsData(proofs[0]);
+    } catch (error) {
+      console.log("we had an error, panic" + error);
     }
+  }
 
-    if (address) {
-      fetchData();
-    }
-  }, [address]);
+  // useEffect(() => {
+  //   if (address) {
+  //     fetchData();
+  //   }
+  // }, [address]);
 
+  console.log(proofsData);
 
-  console.log(proofsData.map((a) => a))
-
-  const { contract: tokenContract } = useContract("0xF5bCF3C61b688616302b14BE21eED9E7B44ab388");
+  // THIS IS THE TOKEN ADDRESS
+  const { contract: tokenContract } = useContract(
+    "0xF5bCF3C61b688616302b14BE21eED9E7B44ab388"
+  );
   const { data: tokenBalance } = useTokenBalance(tokenContract, address);
 
   // Make interaction based on price amount
@@ -59,18 +71,20 @@ const Home: NextPage = () => {
         <ConnectWallet />
         {address && (
           <div>
-            <div style={{
-              backgroundColor: "#222",
-              padding: "2rem",
-              borderRadius: "1rem",
-              textAlign: "center",
-              minWidth: "500px",
-              marginBottom: "2rem",
-              marginTop: "2rem",
-            }}>
+            <div
+              style={{
+                backgroundColor: "#222",
+                padding: "2rem",
+                borderRadius: "1rem",
+                textAlign: "center",
+                minWidth: "500px",
+                marginBottom: "2rem",
+                marginTop: "2rem",
+              }}
+            >
               <h1>Create Merkle Tree</h1>
               <button
-                onClick={() => console.log(fetchData(address))}
+                onClick={() => fetchData()}
                 style={{
                   padding: "1rem",
                   borderRadius: "8px",
@@ -80,32 +94,37 @@ const Home: NextPage = () => {
                   cursor: "pointer",
                   fontSize: "1rem",
                 }}
-              >Generate</button>
-              {merkleRoot && (
-                <p>Merkle Root Hash: {merkleRoot}</p>
-              )}
+              >
+                Generate
+              </button>
+              {merkleRoot && <p>Merkle Root Hash: {merkleRoot}</p>}
             </div>
-            <div style={{
-              backgroundColor: "#222",
-              padding: "2rem",
-              borderRadius: "1rem",
-              textAlign: "center",
-              minWidth: "500px",
-            }}>
+            <div
+              style={{
+                backgroundColor: "#222",
+                padding: "2rem",
+                borderRadius: "1rem",
+                textAlign: "center",
+                minWidth: "500px",
+              }}
+            >
               <h1>$COQWIF Airdrop</h1>
               <h3>Token balance: {tokenBlc}</h3>
               <Web3Button
-                contractAddress="0x8b51B8CAf0d9962Bc6Da46113CAeb5adD2f141E2"
-                action={async (contract) => contract.call(
-                  "claimAirdrop",
-                  [
+                contractAddress="0x7F83d05c59ee6B346EE31675b626bBbf5cc8169a"
+                action={async (contract) =>
+                  contract.call("claimAirdrop", [
                     proofsData, // Use proofsData here
-                    100000000000000000000,
-                  ]
-                )}
-                onError={() => alert("Not eligible for airdrop or already claimed!")}
+                    "100000000000000000000",
+                  ])
+                }
+                onError={() =>
+                  alert("Not eligible for airdrop or already claimed!")
+                }
                 onSuccess={() => alert("Airdrop claimed!")}
-              >Claim Airdrop</Web3Button>
+              >
+                Claim Airdrop
+              </Web3Button>
             </div>
           </div>
         )}
